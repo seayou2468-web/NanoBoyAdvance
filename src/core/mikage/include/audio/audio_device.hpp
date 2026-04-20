553d843326c3088e2f92fa7aee6ae4bc3c59567c
seayou2468-web <seayou2468@gmail.com>
2026-04-20 19:19:33 +0900

    fix: add non-iOS audio device fallback for linux adapter builds

--- src/core/mikage/include/audio/audio_device.hpp
+++ src/core/mikage/include/audio/audio_device.hpp
@@ -1,4 +1,33 @@
 #pragma once
 
+#include "./audio_device_interface.hpp"
+
+#if defined(__APPLE__)
+#include <TargetConditionals.h>
+#endif
+
+#if defined(__APPLE__) && TARGET_OS_IPHONE
 #include "./ios_audio_device.hpp"
 using AudioDevice = IOSAudioDevice;
+#else
+class NoopAudioDevice final : public AudioDeviceInterface {
+  public:
+    explicit NoopAudioDevice(const AudioDeviceConfig& audioSettings)
+        : AudioDeviceInterface(nullptr, audioSettings) {}
+
+    void init(Samples& sampleBuffer, bool /*safe*/ = false) override {
+        samples = &sampleBuffer;
+        running = false;
+    }
+
+    void close() override {
+        running = false;
+        samples = nullptr;
+    }
+
+    void start() override { running = true; }
+    void stop() override { running = false; }
+};
+
+using AudioDevice = NoopAudioDevice;
+#endif
