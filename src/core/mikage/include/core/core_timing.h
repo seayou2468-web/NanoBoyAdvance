791cc446999d6304e76281b440e9441be96012f2
seayou2468-web <seayou2468@gmail.com>
2026-04-23 07:50:02 +0900

    mikage/core: include common_types in core_timing for s64/u64

--- src/core/mikage/include/core/core_timing.h
+++ src/core/mikage/include/core/core_timing.h
@@ -1,3 +1,4 @@
 #pragma once
 #include "../arm_defs.hpp"
+#include "../common/common_types.h"
 namespace Core::Timing { class Timer { public: s64 GetDowncount() const { return 0; } void AddTicks(u64) {} }; }
