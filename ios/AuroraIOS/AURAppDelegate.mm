88799395457f02accfd05e57d81f404f4483d2d3
seayou2468-web <seayou2468@gmail.com>
2026-04-22 14:44:48 +0900

    Redirect iOS app logs to Documents/log.txt

--- ios/AuroraIOS/AURAppDelegate.mm
+++ ios/AuroraIOS/AURAppDelegate.mm
@@ -1,5 +1,24 @@
 #import "AURAppDelegate.h"
 #import "AURSceneDelegate.h"
+#include <cstdio>
+#include <unistd.h>
+
+static void AURSetupLiveDocumentsLogFile(void) {
+    NSString *documentsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+    if (documentsPath.length == 0) {
+        return;
+    }
+
+    NSString *logPath = [documentsPath stringByAppendingPathComponent:@"log.txt"];
+    FILE *logFile = fopen(logPath.fileSystemRepresentation, "a+");
+    if (logFile == nullptr) {
+        return;
+    }
+
+    setvbuf(logFile, nullptr, _IOLBF, 0);
+    dup2(fileno(logFile), STDERR_FILENO);
+    dup2(fileno(logFile), STDOUT_FILENO);
+}
 
 @implementation AURAppDelegate
 
@@ -7,6 +26,8 @@
     didFinishLaunchingWithOptions:(NSDictionary* _Nullable)launchOptions {
     (void)application;
     (void)launchOptions;
+    AURSetupLiveDocumentsLogFile();
+    NSLog(@"[AUR][Log] Live log redirection enabled: Documents/log.txt");
     // ウィンドウ生成は AURSceneDelegate に委譲。
     // iOS 13+ SceneDelegate が有効な場合、ここでは何もしない。
     return YES;
