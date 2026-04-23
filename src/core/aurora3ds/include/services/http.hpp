0ec24235fa08f58a2f83fa81cc8689b8c350336d
seayou2468-web <seayou2468@gmail.com>
2026-04-24 07:38:12 +0900

    Implement stateful HTTP context/request IPC paths

--- src/core/aurora3ds/include/services/http.hpp
+++ src/core/aurora3ds/include/services/http.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <unordered_map>
 #include <unordered_set>
+#include <string>
+#include <vector>
 
 #include "../helpers.hpp"
 #include "../kernel/kernel_types.hpp"
@@ -16,9 +18,24 @@ class HTTPService {
 
 	bool initialized = false;
 	u32 nextRootCertChainHandle = 1;
+	u32 nextContextHandle = 1;
 	std::unordered_map<u32, std::unordered_set<u32>> rootCertChains;
+	struct HTTPContext {
+		u32 method = 0;
+		std::string url;
+		u32 statusCode = 200;
+		std::vector<u8> responseData;
+		u32 readOffset = 0;
+		bool requestStarted = false;
+	};
+	std::unordered_map<u32, HTTPContext> contexts;
 
 	// Service commands
+	void createContext(u32 messagePointer);
+	void closeContext(u32 messagePointer);
+	void beginRequest(u32 messagePointer);
+	void getResponseStatusCode(u32 messagePointer);
+	void receiveData(u32 messagePointer);
 	void createRootCertChain(u32 messagePointer);
 	void initialize(u32 messagePointer);
 	void rootCertChainAddDefaultCert(u32 messagePointer);
