f8ebe8c88605df0c7b1337bae4df0493168456fe
seayou2468-web <seayou2468@gmail.com>
2026-04-24 17:08:24 +0900

    Replace aurora3ds symlinks with integrated source trees

---
+++ src/core/aurora3ds/src/core/hle/service/cam/cam_s.h
@@ -0,0 +1,22 @@
+// Copyright 2015 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included..
+
+#pragma once
+
+#include "core/hle/service/cam/cam.h"
+
+namespace Service::CAM {
+
+class CAM_S final : public Module::Interface {
+public:
+    explicit CAM_S(std::shared_ptr<Module> cam);
+
+private:
+    SERVICE_SERIALIZATION(CAM_S, cam, Module)
+};
+
+} // namespace Service::CAM
+
+SERIALIZATION_CLASS_EXPORT_KEY(Service::CAM::CAM_S)
+SERIALIZATION_CONSTRUCT(Service::CAM::CAM_S)
