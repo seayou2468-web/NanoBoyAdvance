b1ccb5f93df81663b9ee11c841e99dba832d1a1c
seayou2468-web <seayou2468@gmail.com>
2026-04-24 18:51:13 +0900

    Refine Aurora serialization headers with required std includes

--- src/core/aurora3ds/include/aurora_serialization/access.hpp
+++ src/core/aurora3ds/include/aurora_serialization/access.hpp
@@ -1,15 +1,3 @@
 #pragma once
 
 #include "base_object.hpp"
-
-#ifndef AURORA_CLASS_EXPORT
-#define AURORA_CLASS_EXPORT(...) ((void)0)
-#endif
-
-#ifndef AURORA_CLASS_EXPORT_KEY
-#define AURORA_CLASS_EXPORT_KEY(...) ((void)0)
-#endif
-
-#ifndef AURORA_CLASS_EXPORT_IMPLEMENT
-#define AURORA_CLASS_EXPORT_IMPLEMENT(...) ((void)0)
-#endif
--- src/core/aurora3ds/include/aurora_serialization/unordered_map.hpp
+++ src/core/aurora3ds/include/aurora_serialization/unordered_map.hpp
@@ -1,15 +1,5 @@
 #pragma once
 
-#include "base_object.hpp"
+#include <unordered_map>
 
-#ifndef AURORA_CLASS_EXPORT
-#define AURORA_CLASS_EXPORT(...) ((void)0)
-#endif
-
-#ifndef AURORA_CLASS_EXPORT_KEY
-#define AURORA_CLASS_EXPORT_KEY(...) ((void)0)
-#endif
-
-#ifndef AURORA_CLASS_EXPORT_IMPLEMENT
-#define AURORA_CLASS_EXPORT_IMPLEMENT(...) ((void)0)
-#endif
+#include "export.hpp"
