949707b6713d02195f9c17c710bcd570ce9cbbd3
seayou2468-web <seayou2468@gmail.com>
2026-04-24 19:34:24 +0900

    Fix duplicate serialization header definitions

--- src/core/aurora3ds/src/core/sys/common/serialization/std_optional.hpp
+++ src/core/aurora3ds/src/core/sys/common/serialization/std_optional.hpp
@@ -4,36 +4,4 @@
 
 #pragma once
 
-#include <optional>
-#include <utility>
-#include "common/serialization/serialization_alias.hpp"
-
-namespace Serialization {
-
-template <class Archive, class T>
-void save(Archive& ar, const std::optional<T>& value, const unsigned int) {
-    bool has_value = value.has_value();
-    ar & has_value;
-    if (has_value) {
-        ar & *value;
-    }
-}
-
-template <class Archive, class T>
-void load(Archive& ar, std::optional<T>& value, const unsigned int) {
-    bool has_value = false;
-    ar & has_value;
-    if (has_value) {
-        value.emplace();
-        ar & *value;
-    } else {
-        value.reset();
-    }
-}
-
-template <class Archive, class T>
-void serialize(Archive& ar, std::optional<T>& value, const unsigned int version) {
-    split_free(ar, value, version);
-}
-
-} // namespace Serialization
+#include "../../../../../include/common/serialization/std_optional.hpp"
--- src/core/aurora3ds/src/core/sys/common/serialization/std_variant.hpp
+++ src/core/aurora3ds/src/core/sys/common/serialization/std_variant.hpp
@@ -3,40 +3,4 @@
 
 #pragma once
 
-#include <variant>
-#include "common/serialization/serialization_alias.hpp"
-
-namespace Serialization {
-
-template <std::size_t I = 0, class Archive, class... Ts>
-void LoadVariantByIndex(Archive& ar, std::variant<Ts...>& value, std::size_t index) {
-    if constexpr (I < sizeof...(Ts)) {
-        if (index == I) {
-            value.template emplace<I>();
-            ar & std::get<I>(value);
-            return;
-        }
-        LoadVariantByIndex<I + 1>(ar, value, index);
-    }
-}
-
-template <class Archive, class... Ts>
-void save(Archive& ar, const std::variant<Ts...>& value, const unsigned int) {
-    std::size_t index = value.index();
-    ar & index;
-    std::visit([&ar](const auto& v) { ar & v; }, value);
-}
-
-template <class Archive, class... Ts>
-void load(Archive& ar, std::variant<Ts...>& value, const unsigned int) {
-    std::size_t index = 0;
-    ar & index;
-    LoadVariantByIndex(ar, value, index);
-}
-
-template <class Archive, class... Ts>
-void serialize(Archive& ar, std::variant<Ts...>& value, const unsigned int version) {
-    split_free(ar, value, version);
-}
-
-} // namespace Serialization
+#include "../../../../../include/common/serialization/std_variant.hpp"
