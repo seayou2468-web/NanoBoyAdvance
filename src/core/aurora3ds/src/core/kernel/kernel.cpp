a9dd61dd77076f673972ce4f4e39d420bed93463
seayou2468-web <seayou2468@gmail.com>
2026-04-24 11:52:41 +0900

    Batch-map remaining reference SVC IDs into aurora dispatch

--- src/core/aurora3ds/src/core/kernel/kernel.cpp
+++ src/core/aurora3ds/src/core/kernel/kernel.cpp
@@ -31,8 +31,24 @@ Kernel::Kernel(CPU& cpu, Memory& mem, GPU& gpu, const EmulatorConfig& config)
 
 void Kernel::serviceSVC(u32 svc) {
 	switch (svc) {
+		case 0x00:
+			Helpers::warn("Reference SVC stub not yet implemented: %X @ %08X", svc, regs[15]);
+			regs[0] = Result::OS::NotImplemented;
+			break;
 		case 0x01: controlMemory(); break;
 		case 0x02: queryMemory(); break;
+		case 0x03: exitThread(); break;  // ExitProcess (single-process model fallback)
+		case 0x04:
+		case 0x05:
+		case 0x06:
+		case 0x07:
+		case 0x0D:
+		case 0x0E:
+		case 0x10:
+		case 0x12:
+			Helpers::warn("Reference SVC stub not yet implemented: %X @ %08X", svc, regs[15]);
+			regs[0] = Result::OS::NotImplemented;
+			break;
 		case 0x08: createThread(); break;
 		case 0x09: exitThread(); break;
 		case 0x0A: svcSleepThread(); break;
@@ -82,18 +98,136 @@ void Kernel::serviceSVC(u32 svc) {
 		case 0x3A: getResourceLimitCurrentValues(); break;
 		case 0x3B: getThreadContext(); break;
 		case 0x3D: outputDebugString(); break;
+		case 0x3C:
+		case 0x3E:
+		case 0x3F:
+		case 0x40:
+		case 0x41:
+		case 0x42:
+		case 0x43:
+		case 0x44:
+		case 0x45:
+		case 0x46:
+			Helpers::warn("Reference SVC stub not yet implemented: %X @ %08X", svc, regs[15]);
+			regs[0] = Result::OS::NotImplemented;
+			break;
 		case 0x47: createPort(); break;
 		case 0x48: createSessionToPort(); break;
 		case 0x49: createSession(); break;
 		case 0x4A: acceptSession(); break;
+		case 0x4B:
+		case 0x4C:
+		case 0x4D:
+		case 0x4E:
+		case 0x4F:
+		case 0x50:
+		case 0x51:
+		case 0x52:
+		case 0x53:
+		case 0x54:
+		case 0x55:
+		case 0x56:
+		case 0x57:
+		case 0x58:
+		case 0x59:
+		case 0x5A:
+		case 0x5B:
+		case 0x5C:
+		case 0x5D:
+		case 0x5E:
+		case 0x5F:
+		case 0x60:
+		case 0x61:
+		case 0x62:
+		case 0x63:
+		case 0x64:
+		case 0x65:
+		case 0x66:
+		case 0x67:
+		case 0x68:
+		case 0x69:
+		case 0x6A:
+		case 0x6B:
+		case 0x6C:
+		case 0x6D:
+		case 0x6E:
+		case 0x6F:
+		case 0x70:
+		case 0x71:
+		case 0x72:
+		case 0x73:
+		case 0x74:
+		case 0x75:
+		case 0x76:
+		case 0x77:
+		case 0x78:
+		case 0x79:
+		case 0x7A:
+		case 0x7B:
+		case 0x7C:
+		case 0x7D:
+		case 0x7E:
+		case 0x7F:
+		case 0x80:
+		case 0x81:
+		case 0x82:
+		case 0x83:
+		case 0x84:
+		case 0x85:
+		case 0x86:
+		case 0x87:
+		case 0x88:
+		case 0x89:
+		case 0x8A:
+		case 0x8B:
+		case 0x8C:
+		case 0x8D:
+		case 0x8E:
+		case 0x8F:
+		case 0x90:
+			Helpers::warn("Reference SVC stub not yet implemented: %X @ %08X", svc, regs[15]);
+			regs[0] = Result::OS::NotImplemented;
+			break;
 
 		// Luma SVCs
 		case 0x91: regs[0] = Result::Success; break;  // FlushDataCacheRange (no-op for now)
 		case 0x92: regs[0] = Result::Success; break;  // FlushEntireDataCache (no-op for now)
 		case 0x93: svcInvalidateInstructionCacheRange(); break;
 		case 0x94: svcInvalidateEntireInstructionCache(); break;
+		case 0x95:
+		case 0x96:
+		case 0x97:
+		case 0x98:
+		case 0x99:
+		case 0x9A:
+		case 0x9B:
+		case 0x9C:
+		case 0x9D:
+		case 0x9E:
+		case 0x9F:
+		case 0xA0:
+		case 0xA1:
 		case 0xA2: controlMemory(); break;  // ControlMemoryEx
 		case 0xA3: controlMemory(); break;  // ControlMemoryUnsafe
+		case 0xA4:
+		case 0xA5:
+		case 0xA6:
+		case 0xA7:
+		case 0xA8:
+		case 0xA9:
+		case 0xAA:
+		case 0xAB:
+		case 0xAC:
+		case 0xAD:
+		case 0xAE:
+		case 0xAF:
+		case 0xB0:
+		case 0xB1:
+		case 0xB2:
+		case 0xB3:
+			Helpers::warn("Reference SVC stub not yet implemented: %X @ %08X", svc, regs[15]);
+			regs[0] = Result::OS::NotImplemented;
+			break;
 		default:
 			Helpers::warn("Unimplemented svc: %X @ %08X", svc, regs[15]);
 			regs[0] = Result::OS::NotImplemented;
